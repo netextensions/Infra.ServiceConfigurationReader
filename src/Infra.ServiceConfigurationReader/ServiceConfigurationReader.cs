ac1210432b4c3445c86e9edf99487376f89f23bb
Csaba Salat <csalat@globalblue.com>
2020-04-25 20:26:27 +0200

    init

---
+++ src/Infra.ServiceConfigurationReader/ServiceConfigurationReader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace NetExtensions
+{
+    public class ServiceConfigurationReader
+    {
+        private const string AppSettings = "appsettings.json";
+        public static IConfiguration CreateConfiguration(params string[] jsonFileNames)
+        {
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile(AppSettings, false, true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", true)
+                .AddEnvironmentVariables();
+
+            foreach (var fileName in jsonFileNames)
+                builder.AddJsonFile(fileName);
+
+            return builder.Build();
+        }
+    }
+}
